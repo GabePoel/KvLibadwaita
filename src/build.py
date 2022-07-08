152c204504ea32fb3f4917059d77e5db73ee5eb2
GabePoel <gabrielpoel@gmail.com>
2022-07-07 22:58:19 -0700

    Beginnings of color assignment.

--- src/build.py
+++ src/build.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import os
 import shutil
+import json
 
 here = os.getcwd()
 home = os.path.expanduser('~')
@@ -11,13 +12,49 @@ svg_src = os.path.join(src, 'KvLibadwaitaDraft.svg')
 kvc_src = os.path.join(src, 'KvLibadwaitaDraft.kvconfig')
 svg_dst = os.path.join(dst, 'KvLibadwaitaDraft.svg')
 kvc_dst = os.path.join(dst, 'KvLibadwaitaDraft.kvconfig')
+svg_l_dst = os.path.join(dst, 'KvLibadwaitaDraft.svg')
+svg_d_dst = os.path.join(dst, 'KvLibadwaitaDraftDark.svg')
+kvc_l_dst = os.path.join(dst, 'KvLibadwaitaDraft.kvconfig')
+kvc_d_dst = os.path.join(dst, 'KvLibadwaitaDraftDark.kvconfig')
 
 os.makedirs(dst, exist_ok=True)
 shutil.copy(svg_src, svg_dst)
-shutil.copy(kvc_src, kvc_dst)
+shutil.copy(kvc_src, kvc_l_dst)
+shutil.copy(kvc_src, kvc_d_dst)
 
 with open(svg_dst, 'r') as f:
     s = f.read()
 s = s.replace('#00037f', 'none', -1)
 with open(svg_dst, 'w') as f:
     f.write(s)
+    
+# with open(os.path.join(here, 'color-assignment.json'), 'r') as f:
+#     colors = json.load(f)
+# new_colors = dict()
+# for h in colors:
+#     c = colors[h]
+#     new_colors[c] = dict()
+#     new_colors[c]["ref"] = h
+#     new_colors[c]["light"] = h
+#     new_colors[c]["dark"] = h
+# with open(os.path.join(here, 'colors.json'), 'w') as f:
+#     json.dump(new_colors, f)
+
+with open(os.path.join(here, 'colors.json'), 'r') as f:
+    colors = json.load(f)
+with open(svg_src, 'r') as f:
+    svg_l_str = f.read()
+with open(svg_src, 'r') as f:
+    svg_d_str = f.read()
+for color in colors:
+    ref = colors[color]["ref"]
+    light = colors[color]["light"]
+    dark = colors[color]["dark"]
+    svg_l_str = svg_l_str.replace(ref, light, -1)
+    svg_d_str = svg_d_str.replace(ref, dark, -1)
+svg_l_str = svg_l_str.replace('#00037f', 'none', -1)
+svg_d_str = svg_d_str.replace('#00037f', 'none', -1)
+with open(svg_l_dst, 'w') as f:
+    f.write(svg_l_str)
+with open(svg_d_dst, 'w') as f:
+    f.write(svg_d_str)
