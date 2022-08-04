e687b334f16701e0dbffa68bac899f80451ae738
GabePoel <gabrielpoel@gmail.com>
2022-08-04 13:19:20 -0700

    First release of "draft" version!
    Theme has been completely redone from scratch to be easier to fix lots of bugs, be easier to maintain, and better match libadwaita!

--- dev/build.py
+++ dev/build.py
@@ -12,15 +12,15 @@ svg_src = os.path.join(src, 'KvLibadwaitaDraft.svg')
 kvc_src = os.path.join(src, 'KvLibadwaitaDraft.kvconfig')
 svg_dst = os.path.join(dst, 'KvLibadwaitaDraft.svg')
 kvc_dst = os.path.join(dst, 'KvLibadwaitaDraft.kvconfig')
-svg_l_dst = os.path.join(dst, 'KvLibadwaitaDraft.svg')
-svg_d_dst = os.path.join(dst, 'KvLibadwaitaDraftDark.svg')
-kvc_l_dst = os.path.join(dst, 'KvLibadwaitaDraft.kvconfig')
-kvc_d_dst = os.path.join(dst, 'KvLibadwaitaDraftDark.kvconfig')
+svg_l_dst = os.path.join(dst, 'KvLibadwaita.svg')
+svg_d_dst = os.path.join(dst, 'KvLibadwaitaDark.svg')
+kvc_l_dst = os.path.join(dst, 'KvLibadwaita.kvconfig')
+kvc_d_dst = os.path.join(dst, 'KvLibadwaitaDark.kvconfig')
 
 os.makedirs(dst, exist_ok=True)
 shutil.copy(svg_src, svg_dst)
-shutil.copy(kvc_src, kvc_l_dst)
-shutil.copy(kvc_src, kvc_d_dst)
+# shutil.copy(kvc_src, kvc_l_dst)
+# shutil.copy(kvc_src, kvc_d_dst)
 
 with open(svg_dst, 'r') as f:
     s = f.read()
@@ -46,15 +46,25 @@ with open(svg_src, 'r') as f:
     svg_l_str = f.read()
 with open(svg_src, 'r') as f:
     svg_d_str = f.read()
+with open(kvc_src, 'r') as f:
+    kvc_l_str = f.read()
+with open(kvc_src, 'r') as f:
+    kvc_d_str = f.read()
 for color in colors:
     ref = colors[color]["ref"]
     light = colors[color]["light"]
     dark = colors[color]["dark"]
     svg_l_str = svg_l_str.replace(ref, light, -1)
     svg_d_str = svg_d_str.replace(ref, dark, -1)
+    kvc_l_str = kvc_l_str.replace(ref, light, -1)
+    kvc_d_str = kvc_d_str.replace(ref, dark, -1)
 svg_l_str = svg_l_str.replace('#00037f', 'none', -1)
 svg_d_str = svg_d_str.replace('#00037f', 'none', -1)
 with open(svg_l_dst, 'w') as f:
     f.write(svg_l_str)
 with open(svg_d_dst, 'w') as f:
     f.write(svg_d_str)
+with open(kvc_l_dst, 'w') as f:
+    f.write(kvc_l_str)
+with open(kvc_d_dst, 'w') as f:
+    f.write(kvc_d_str)
